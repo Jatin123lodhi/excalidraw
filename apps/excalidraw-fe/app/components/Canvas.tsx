a03bef95186cba368d4c0fe11924a4c4afe794b3
Jatin Lodhi <jatinlodhi111@gmail.com>
2025-02-05 21:51:36 +0530

    added the game class instance logic

--- apps/excalidraw-fe/app/components/Canvas.tsx
+++ apps/excalidraw-fe/app/components/Canvas.tsx
@@ -1,18 +1,79 @@
-import { useEffect, useRef } from "react"
-import { initDraw } from "../draw";
+import { useEffect, useRef, useState } from "react";
+import { IconButton } from "./IconButton";
+import { Circle, Pencil, RectangleHorizontalIcon } from "lucide-react";
+import { Game } from "../draw/game";
+import { deleteAllShapes } from "../draw/http";
 
-export default function Canvas({ roomId, socket }: { roomId: string, socket: WebSocket }) {
-    const canvasRef = useRef<HTMLCanvasElement>(null)
+export type Tool = "circle" | "rect" | "pencil";
+
+export function Canvas({
+    roomId,
+    socket
+}: {
+    socket: WebSocket;
+    roomId: string;
+}) {
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const [game, setGame] = useState<Game>();
+    const [selectedTool, setSelectedTool] = useState<Tool>("circle")
 
     useEffect(() => {
-        if (canvasRef.current) {
-            const canvas = canvasRef.current
-            canvas.width = window.innerWidth;
-            canvas.height = window.innerHeight;
-            initDraw(canvasRef.current, roomId, socket)
-        }
-    }, [canvasRef])
+        game?.setTool(selectedTool);
+    }, [selectedTool, game]);
 
-    return <canvas ref={canvasRef} style={{ backgroundColor: 'black' }}>
-    </canvas>
+    useEffect(() => {
+
+        if (canvasRef.current) {
+            const g = new Game(canvasRef.current, roomId, socket);
+            setGame(g);
+
+            return () => {
+                g.destroy();
+            }
+        }
+
+
+    }, [canvasRef]);
+
+    const clearCanvas = async () => {
+        await deleteAllShapes(roomId)
+        game?.deleteCanvas()
+    }
+
+    return <div style={{
+        height: "100vh",
+        overflow: "hidden"
+    }}>
+        <canvas ref={canvasRef} width={window.innerWidth} height={window.innerHeight} style={{backgroundColor:'black'}}></canvas>
+        <Topbar setSelectedTool={setSelectedTool} selectedTool={selectedTool} clearCanvas={clearCanvas} />
+    </div>
+}
+
+function Topbar({selectedTool, setSelectedTool, clearCanvas}: {
+    selectedTool: Tool,
+    setSelectedTool: (s: Tool) => void
+    clearCanvas: () => void
+}) {
+    return <div style={{
+            position: "fixed",
+            top: 10,
+            left: 10
+        }}>
+            <div className="flex gap-t">
+                <IconButton 
+                    onClick={() => {
+                        setSelectedTool("pencil")
+                    }}
+                    activated={selectedTool === "pencil"}
+                    icon={<Pencil />}
+                />
+                <IconButton onClick={() => {
+                    setSelectedTool("rect")
+                }} activated={selectedTool === "rect"} icon={<RectangleHorizontalIcon />} ></IconButton>
+                <IconButton onClick={() => {
+                    setSelectedTool("circle")
+                }} activated={selectedTool === "circle"} icon={<Circle />}></IconButton>
+                <button style={{color: 'white'}} onClick={clearCanvas}>Delete canvas</button>
+            </div>
+        </div>
 }
